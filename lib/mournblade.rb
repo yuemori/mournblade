8629e2ca24c1fc22615bd0ce590e2dd1abcb6032
yuemori <yuemori@aiming-inc.com>
2017-03-23 02:08:36 +0900

    First implementation

--- lib/mournblade.rb
+++ lib/mournblade.rb
@@ -1,5 +1,28 @@
 require 'mournblade/version'
+require 'curses'
 
 module Mournblade
-  # Your code goes here...
+  class << self
+    def start
+      Curses.init_screen
+
+      begin
+        window = create_window
+        window.getch
+        window.close
+      ensure
+        Curses.close_screen
+      end
+    end
+
+    private
+
+    def create_window
+      Curses::Window.new(0, 0, 0, 0).tap do |window|
+        window.setpos(0, 0)
+        window.addstr('hello, world!')
+        window.refresh
+      end
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,18 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'mournblade'
 
-require 'minitest/autorun'
+module Curses
+  class << self
+    %i(init_screen close_screen).each do |name|
+      define_method(name) { |*_args| }
+    end
+  end
+
+  class Window
+    %i(initialize setpos addstr refresh getch close).each do |name|
+      define_method(name) { |*_args| }
+    end
+  end
+end
+
+Mournblade.include Curses
